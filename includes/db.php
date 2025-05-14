bac0fd7ed11924ab9898b4bb83d13ea9839917a5
Byreds242 <ethan.seisdedos@estudiant.fjaverianas.com>
2025-05-14 20:06:14 +0200

    volvemos

--- includes/db.php
+++ includes/db.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "bender10";
+$password = "";
 $dbname = "e-commerce";
 
 // Crear conexión
@@ -11,9 +11,5 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
-<<<<<<< HEAD
-//echo "Conexión exitosa";
-=======
 echo "Conexión exitosa";
->>>>>>> origin/login
 ?>
