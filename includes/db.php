6e344928e17110b29e923a64298db762cc479037
SebastianCarSal <sebas.salazarl@gmail.com>
2025-05-14 20:05:06 +0200

    volviendo

--- includes/db.php
+++ includes/db.php
@@ -11,9 +11,5 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
-<<<<<<< HEAD
 //echo "Conexión exitosa";
-=======
-echo "Conexión exitosa";
->>>>>>> origin/login
 ?>
